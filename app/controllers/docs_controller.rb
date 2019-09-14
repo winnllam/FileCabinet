bc39b06d218dc005a07a2ba160629e5045c10a71
Winnie <winnieee.lam00@gmail.com>
2019-09-14 18:41:04 -0400

    add docs views and define actions

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,15 +1,25 @@
 class DocsController < ApplicationController
+  before_action :find_doc, only: [:show, :edit, :update, :destory] #runs first; find doc for specific actions
 
   def index
+    @docs = Doc1.all.order("created_at DESC") #order by creation time (descending)
   end
 
   def show
   end
 
   def new
+    @doc = Doc.new #instance of a new document
   end
 
   def create
+    @doc = Doc.new(doc_params) #title and content (in this case)
+
+    if @doc.save
+      redirect_to @doc #redirect to document itself
+    else
+      render 'new' #failed to do it the first time, do it again
+    end
   end
 
   def edit
@@ -24,9 +34,11 @@ class DocsController < ApplicationController
   private
 
     def find_doc
+      @doc = Doc.find(params[:id]) #need to find document to show and delete; fetch using id number
     end
 
-    def doc_params
+    def doc_params #doc attributes from model
+      params.require(:doc).permit(:title, :content)
     end
-    
+
 end
