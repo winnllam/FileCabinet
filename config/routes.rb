1ad12421b7b31ddbed1d072b06b8d04f12b0db4d
Winnie <winnieee.lam00@gmail.com>
2019-09-14 19:48:38 -0400

    change root of application for authenticated user

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
 
-  root 'welcome#index' #make it the root page (won't use /welceom/index)
-
   resources :docs #create all routes we're going to add to application
+  
+  authenticated :user do #for already logged in user
+    root "docs#index", as: "authenticated_root" #go to authenticated_root
+  end
+
+  root 'welcome#index' #make it the root page (won't use /welceom/index) for people who have not logged in
+
 end
 
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
