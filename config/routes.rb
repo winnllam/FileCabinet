3a79daa0daea761aff500ecfee08073db6807074
Winnie <winnieee.lam00@gmail.com>
2019-09-14 17:21:17 -0400

    generate welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'welcome/index'
+  
+  root 'welcome#index' #make it the root page (won't use /welceom/index)
 end
+
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
