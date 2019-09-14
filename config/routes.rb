183b48e90c4ec7c7d2b78b8a9b5d8ea3a1fe1cf8
Winnie <winnieee.lam00@gmail.com>
2019-09-14 18:04:48 -0400

    Doc controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  
+
   root 'welcome#index' #make it the root page (won't use /welceom/index)
+
+  resources :docs #create all routes we're going to add to application
 end
 
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
